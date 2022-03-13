a0e0e9713a6e101f3e43dd7227f514fe55e0ce14
Shoban07 <shobankumar.r96@gmail.com>
2022-03-13 19:26:22 +0530

    Initial Commit

---
+++ ProjectManagementSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectManagementSystem
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello  PRMS World!");
+        }
+    }
+}
