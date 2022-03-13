5e950bc82d699e3b6a26221d997da6846fd21ad2
Shoban07 <shobankumar.r96@gmail.com>
2022-03-13 21:49:23 +0530

    Justification for status enum

--- ProjectManagementSystem/Status.cs
+++ ProjectManagementSystem/Status.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 namespace ProjectManagementSystem
 {
+    /*
+     1. To Avoid status name conflicts
+     2. To have clear overview on what was the current status*/
     public enum Status
     {
         Active,
