b1ba368e689b31acf3764328a4a4c7d7f438703b
Shoban07 <shobankumar.r96@gmail.com>
2022-03-13 21:52:10 +0530

    justification on enum ResourceAvailable

--- ProjectManagementSystem/ResourceAvailable.cs
+++ ProjectManagementSystem/ResourceAvailable.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 namespace ProjectManagementSystem
 {
+    /* Note: 
+     1. Avoid conflicts on resource names
+     2. Resource names are named after developemnt purpose will have real names of user / resource name while moving from dev to QA environment
+    */
     public enum ResourceAvailable
     {
         Res_A,
